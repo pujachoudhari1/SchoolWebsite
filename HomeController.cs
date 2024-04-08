9716bb6c54ff5253dd23b71a1ef8c629628b6397
pujachoudhari1 <129393847+pujachoudhari1@users.noreply.github.com>
2024-04-08 22:19:21 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseFirstApproach.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Events() { return View(); }
+
+        public ActionResult Staff() { return View(); }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult AdminLogin()
+        {
+            ViewBag.Message = "Only for Staff";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Welcome";
+
+            return View();
+        }
+        public ActionResult Gallary()
+        {
+           
+            return View();
+        }
+        public ActionResult FeedBack()
+        {
+
+            return View();
+        }
+        public ActionResult nav()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ RegisterModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseFirstApproach.Models
+{
+    
+        [Table("Register")]
+        public class RegisterModel
+        {
+
+            // use [key] if primary key 
+            // auto increment - use dataannotation [databaseGenertaed(databaseGeneratedOption.Identity)]
+            [Key]
+            public int ID { get; set; }
+            public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Mobile { get; set; } 
+        public bool Gender { get; set; }
+
+        public string Age { get; set; }
+        public string UserName { get; set; }
+        
+          public DateTime JoiningDate { get; set; }
+        public string Qualification { get; set; }
+        public int PassWord { get; set; }
+        public byte image { get; set; }
+
+    }
+        public class LoginModel
+        {
+
+            // use [key] if primary key 
+            // auto increment - use dataannotation [databaseGenertaed(databaseGeneratedOption.Identity)]
+
+            public string UserName { get; set; }
+            public int PassWord { get; set; }
+
+
+        }
+       
+    }
+
+
